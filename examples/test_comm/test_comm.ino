7223cdc3833ad250d238022193210356bb6f6cb4
Sasha Lopoukhine <superlopuh@gmail.com>
2017-03-22 15:03:34 +0100

    replace print function with macro in

--- examples/test_comm/test_comm.ino
+++ examples/test_comm/test_comm.ino
@@ -10,39 +10,6 @@ void loop() {
     int next = Serial.read();
     SLState newState = SLStateNone;
     UPDATE_STATE(&newState, next);
-    printState(newState);
-  }
-}
-
-void printState(SLState state) {
-  switch (state) {
-    case SLStateNone:
-      Serial.println("SLStateNone");
-      break;
-    case SLStateWakingUp:
-      Serial.println("SLStateWakingUp");
-      break;
-    case SLStateStandby:
-      Serial.println("SLStateStandby");
-      break;
-    case SLStateListening:
-      Serial.println("SLStateListening");
-      break;
-    case SLStateLoading:
-      Serial.println("SLStateLoading");
-      break;
-    case SLStateYes:
-      Serial.println("SLStateYes");
-      break;
-    case SLStateError:
-      Serial.println("SLStateError");
-      break;
-    case SLStateShuttingDown:
-      Serial.println("SLStateShuttingDown");
-      break;
-    default:
-      // Should never happen
-      Serial.println("unkown");
-      while (true) {}
+    PRINT_STATE(newState);
   }
 }
