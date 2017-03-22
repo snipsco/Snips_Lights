83526907001bced3b6db5dc517ec9b6e7ad8aa05
Sasha Lopoukhine <superlopuh@gmail.com>
2017-03-22 14:59:13 +0100

    replace print function with macro in home.ino

--- examples/home/home.ino
+++ examples/home/home.ino
@@ -1,6 +1,8 @@
 #include "Adafruit_NeoPixel.h"
 #include "Snips_Lights.h"
 
+#define DEBUG false
+
 #define PIXELS_WHITE false
 #define PIXELS 24
 #define PIN 6
@@ -21,48 +23,15 @@ void setup() {
   lights->getPixels()->setBrightness(40);
 }
 
-SLTimeInterval startTime = millis();
-
 void loop() {
   if (Serial.available()) {
     int next = Serial.read();
     SLState newState = SLStateNone;
     UPDATE_STATE(&newState, next);
     lights->setState(newState);
-    printState(newState);
+    #if DEBUG
+      PRINT_STATE(newState);
+    #endif
   }
   lights->step();
-}
-
-void printState(SLState state) {
-  switch (state) {
-    case SLStateNone:
-      Serial.println("SLStateNone");
-      break;
-    case SLStateWakingUp:
-      Serial.println("SLStateWakingUp");
-      break;
-    case SLStateStandby:
-      Serial.println("SLStateStandby");
-      break;
-    case SLStateListening:
-      Serial.println("SLStateListening");
-      break;
-    case SLStateLoading:
-      Serial.println("SLStateLoading");
-      break;
-    case SLStateYes:
-      Serial.println("SLStateYes");
-      break;
-    case SLStateError:
-      Serial.println("SLStateError");
-      break;
-    case SLStateShuttingDown:
-      Serial.println("SLStateShuttingDown");
-      break;
-    default:
-      // Should never happen
-      Serial.println("unkown");
-      while (true) {}
-  }
 }
