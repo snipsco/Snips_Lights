af8210e932a766fd8f009916f63de9ff8ca7f234
Sasha Lopoukhine <superlopuh@gmail.com>
2017-03-27 16:13:01 +0200

    remove macros for PIXEL_TYPE

--- examples/test_all/test_all.ino
+++ examples/test_all/test_all.ino
@@ -1,17 +1,13 @@
 #include "Adafruit_NeoPixel.h"
 #include "Snips_Lights.h"
 
-#define PIXELS_WHITE false
+#define PIXELS_WHITE true
 #define PIXELS 24
 #define PIN 6
 
-//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
-//   NEO_RGBW    Pixels are wired for GRBW bitstream (NeoPixel GRBW products)
 #if PIXELS_WHITE
-  #define PIXEL_TYPE NEO_GRBW + NEO_KHZ800
   #define WHITE_COLOR(pixels, value) pixels->Color(0, 0, 0, value)
 #else
-  #define PIXEL_TYPE NEO_GRB + NEO_KHZ800
   #define WHITE_COLOR(pixels, value) pixels->Color(value, value, value)
 #endif
 
@@ -19,7 +15,7 @@ Snips_Lights *lights;
 
 void setup() {
   Serial.begin(9600);
-  lights = new Snips_Lights(PIXELS, PIN, PIXEL_TYPE);
+  lights = new Snips_Lights(PIXELS, PIN, NEO_KHZ800 + NEO_GRBW);
   lights->setPrimaryColor(WHITE_COLOR(lights->getPixels(), 255));
   lights->getPixels()->setBrightness(40);
   lights->setState(SLStateWakingUp);
